f9767e01061e7fa8db8c1fb59cd638efcda83309
Salil Dureja <sdureja@wisc.edu>
2017-04-07 01:44:05 -0500

    Mutex implemented, doesn't compile

--- xv6/user/mutex.c
+++ xv6/user/mutex.c
@@ -4,15 +4,37 @@
 
 void mutex_init(struct mutex* mtx)
 {
-	/* fill this in! */
+	mtx->flag  = 0;
+	mtx->guard = 0;
+    queue_init(mtx->q);
 }
 
+// TODO remove comments
 void mutex_lock(struct mutex* mtx)
 {
-	/* fill this in! */
+	while(xchg(&mtx->guard, 1) != 0);
+	//while (TestAndSet(&mtx->guard, 1) == 1) ; //acquire guard lock by spinning
+    if (mtx->flag == 0) {
+        mtx->flag = 1;
+		mtx->guard = 0; 
+	}
+	else {
+        queue_add(mtx->q, gettid());
+        setpark();
+        mtx->guard = 0;
+        park();
+    }
 }
 
 void mutex_unlock(struct mutex* mtx)
 {
-	/* fill this in! */
+	while (xchg(&mtx->guard, 1) != 0);
+	//while (TestAndSet(&mtx->guard, 1) == 1); //acquire guard lock by spinning
+    if (queue_empty(mtx->q)) {
+        mtx->flag = 0; // let go of lock; no one wants it
+    }
+    else {
+        unpark(queue_remove(mtx->q)); // hold lock (for next thread!)
+    }
+    mtx->guard = 0;
 }
--- xv6/user/mutex.h
+++ xv6/user/mutex.h
@@ -3,12 +3,15 @@
 #define MUTEX_H
 
 struct mutex {
-	/* fill this in! */
+	// TODO remove comment
+	//pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+	int flag;
+	int guard;
+    queue_t *q;
 };
 
 void mutex_init(struct mutex* mtx);
 void mutex_lock(struct mutex* mtx);
 void mutex_unlock(struct mutex* mtx);
-
 
 #endif
