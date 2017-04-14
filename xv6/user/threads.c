74930a20f251494785a8d153ed9fe3d557bea826
zostale <shyamalanadkat@gmail.com>
2017-04-13 22:31:34 -0500

    add locks in threads.c

--- xv6/user/threads.c
+++ xv6/user/threads.c
@@ -5,16 +5,23 @@
 
 //****@Author : salil, shyamal****
 #define PGSIZE 4096
+static struct spinlock tclock;
+static struct spinlock tjlock;
 
 //THREAD CREATE
 int thread_create(void (*thfunc)(void*), void* arg)
 {
+  if (!tclock.init) {spin_init(&tclock);}
+  //lock
+  spin_lock(&tclock);
   
   void *ustack;
+
   ustack = malloc(2*PGSIZE); //4096 * 2
 
   //if malloc fails
   if (ustack == 0) {
+    spin_unlock(&tclock);
     return -1; 
   }
   
@@ -25,7 +32,7 @@ int thread_create(void (*thfunc)(void*), void* arg)
 
   int pid = clone(thfunc, arg, (void*)ustack);
   //check if pid is neg ??
-
+  spin_unlock(&tclock);
   return pid;
 }
 
@@ -33,14 +40,19 @@ int thread_create(void (*thfunc)(void*), void* arg)
 int thread_join(void)
 {
 
+  if (!tjlock.init) {spin_init(&tjlock);}
+  spin_lock(&tjlock);
+
   void *ustack;
   int retVal =  join((&ustack));
 
   //if join fails/pid is negative
   if(retVal == -1) {
+    spin_unlock(&tjlock);
     return -1;
   }
 
   free(ustack);
+  spin_unlock(&tjlock);
   return retVal;
 }
